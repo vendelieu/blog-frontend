b8c187e2229b9e52339808c0dc9056f51d4f42d3
vendelieu <vennywise>
2022-08-09 05:17:22 +0300

    add projects meta initialization

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -3,6 +3,9 @@ import { GithubRepo } from '../../interfaces/github-repo';
 import { GithubService } from '../../services/github.service';
 import { faCode, faTerminal } from '@fortawesome/free-solid-svg-icons';
 import { Options } from '../../config/site-options';
+import { MetaService } from '../../core/meta.service';
+import { HTMLMetaData } from '../../interfaces/meta';
+import { OptionEntity } from '../../interfaces/options';
 
 @Component({
   selector: 'app-projects',
@@ -10,19 +13,32 @@ import { Options } from '../../config/site-options';
   styleUrls: ['./projects.component.less']
 })
 export class ProjectsComponent implements OnInit {
+  private options: OptionEntity = Options;
   loading = false;
   githubProjects: GithubRepo[] | undefined = undefined;
   projectIcon = faCode;
   langIcon = faTerminal;
-  email = Options.email;
+  email = this.options['email'];
 
-  constructor(private githubService: GithubService) {}
+  constructor(private githubService: GithubService, private metaService: MetaService) {
+  }
 
   public ngOnInit() {
+    this.initMeta();
     this.loading = true;
     this.githubService.getApiProjects().subscribe((res) => {
       this.githubProjects = res;
       this.loading = false;
     });
+  }
+
+  private initMeta() {
+    const metaData: HTMLMetaData = {
+      title: this.options['site_name'] + ' - ' + 'Projects',
+      description: this.options['site_description'],
+      author: this.options['site_author'],
+      keywords: this.options['site_keywords']
+    };
+    this.metaService.updateHTMLMeta(metaData);
   }
 }

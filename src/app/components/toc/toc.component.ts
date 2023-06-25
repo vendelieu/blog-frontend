69fd6df4737d570c00152ce100ab57e970827745
vendelieu <vendelieu@gmail.com>
2023-06-25 03:30:36 +0300

    add svg line progress for toc

--- src/app/components/toc/toc.component.ts
+++ src/app/components/toc/toc.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Inject, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, Input, ViewChild } from '@angular/core';
 import { NodeEl, TocElement } from '../../interfaces/posts';
 import { DOCUMENT } from '@angular/common';
 import { PlatformService } from '../../core/platform.service';
@@ -14,17 +14,31 @@ export class TocComponent implements AfterViewInit {
   @Input('baseUrl') baseUrl!: string;
 
   private intersectionObserver: IntersectionObserver | undefined;
+  @ViewChild('tocPath') tocPathEl!: ElementRef;
 
   constructor(@Inject(DOCUMENT) private document: Document, private platform: PlatformService) {
     if (this.platform.isBrowser) {
       this.intersectionObserver = new IntersectionObserver(
         (entries) => {
           entries.forEach((el) => {
-            if (el.isIntersecting)
-              this.document.getElementById('toc-' + el.target.id)?.classList.add('toc-active');
-            else
-              this.document.getElementById('toc-' + el.target.id)?.classList.remove('toc-active');
+            const tocElement = this.document.getElementById('toc-' + el.target.id);
+            if (el.isIntersecting) {
+              tocElement?.classList.add('toc-active');
+            } else {
+              tocElement?.classList.remove('toc-active');
+            }
           });
+          const intersected = this.document.getElementsByClassName('toc-active');
+          const firstRect = (intersected[0] as HTMLElement).getBoundingClientRect();
+          const lastRect = (
+            intersected[intersected.length - 1] as HTMLElement
+          ).getBoundingClientRect();
+
+          const d = `M ${firstRect.left} ${firstRect.top + firstRect.height - 25} L ${
+            lastRect.left
+          } ${lastRect.top + lastRect.height}`;
+
+          this.tocPathEl.nativeElement.setAttribute('d', d);
         },
         {
           root: null,

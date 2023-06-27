e960d73e5c94e0f67ef3b6c9ae8d2241c081f6eb
vendelieu <vendelieu@gmail.com>
2023-06-27 22:02:27 +0300

    try to move transferstate into resolver

--- src/app/resolvers/post.resolver.ts
+++ src/app/resolvers/post.resolver.ts
@@ -1,11 +1,19 @@
 import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
 import { PostEntity } from '../interfaces/posts';
-import { PostsService } from '../services/posts.service';
+import { TransferHttpService } from '../services/transfer-http.service';
+import { HttpResponseEntity } from '../interfaces/http-response';
+import { ApiUrl } from '../config/api-url';
+import { map } from 'rxjs/operators';
 
 export const postResolver: ResolveFn<PostEntity | undefined> = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
-  return inject(PostsService).getPostBySlug(route.paramMap.get('postSlug')!);
+  const http = inject(TransferHttpService);
+  return http
+    .get<HttpResponseEntity<PostEntity>>(
+      http.getApiUrl(ApiUrl.GET_POST_BY_SLUG).replace(':slug', route.paramMap.get('postSlug')!)
+    )
+    .pipe(map((res) => res?.data || undefined));
 };

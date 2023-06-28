ce2744ee6b72ee0fc715c03b9789afa5f8c9dd4e
vendelieu <vendelieu@gmail.com>
2023-06-28 03:43:53 +0300

    change admin check url

--- src/app/core/admin.guard.ts
+++ src/app/core/admin.guard.ts
@@ -1,13 +1,13 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { ApiUrl } from '../config/api-url';
 import { tap } from 'rxjs';
 import { TransferHttpService } from '../services/transfer-http.service';
+import { Options } from '../config/site-options';
 
 export const adminGuard = () => {
   const router = inject(Router);
   const apiService = inject(TransferHttpService);
-  return apiService.checkAccess(apiService.getApiUrl(ApiUrl.ADMIN_STATUS_URL)).pipe(
+  return apiService.checkAccess(`${Options.api_url}:8080/api/admin/check`).pipe(
     tap((value) => {
       return !value ? router.navigate(['/']) : true;
     })

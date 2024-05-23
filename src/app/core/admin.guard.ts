27c5b72a9e3312d36dd1a69165ec981080916fb7
vendelieu <vendelieu@gmail.com>
2024-05-23 09:09:27 +0300

    fix guard

--- src/app/core/admin.guard.ts
+++ src/app/core/admin.guard.ts
@@ -1,13 +1,12 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiUrl } from '../config/api-url';
-import { of, tap } from 'rxjs';
+import { tap } from 'rxjs';
 import { TransferHttpService } from '../services/transfer-http.service';
 import { CoolLocalStorage } from '@angular-cool/storage';
 import { Options } from '../config/site-options';
 
 export const adminGuard = () => {
-  return of(true);
   const router = inject(Router);
   const apiService = inject(TransferHttpService);
   const storage = inject(CoolLocalStorage);

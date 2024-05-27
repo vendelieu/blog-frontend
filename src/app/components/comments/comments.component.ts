2e614d986f81ef19fada50691766b8979268c6a6
vendelieu <vendelieu@gmail.com>
2024-05-27 23:35:10 +0300

    use direct fun

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,10 +1,11 @@
-import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, Inject, Input, PLATFORM_ID, Renderer2, ViewChild } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
 
 import 'giscus';
 import { collectResultSync } from '@lit-labs/ssr/lib/render-result';
 import { html, render } from '@lit-labs/ssr';
 import { PlatformService } from '../../core/platform.service';
+import { isPlatformServer } from '@angular/common';
 
 @Component({
   selector: 'app-comments',
@@ -19,29 +20,32 @@ export class CommentsComponent {
 
   constructor(
     private renderer: Renderer2,
-    private platform: PlatformService,
+    @Inject(PLATFORM_ID) _platformId: Object,
     themeService: ThemeService
   ) {
     this.isExpanded = false;
-    const giscusComponent = collectResultSync(render(html`
-      <giscus-widget
-        id='comments'
-        repo='vendelieu/blog-frontend'
-        repoid='R_kgDOHo0zig'
-        category='Comments'
-        categoryid='DIC_kwDOHo0zis4CXkRl'
-        mapping='og:title'
-        data-strict='1'
-        reactionsenabled='1'
-        emitmetadata='0'
-        inputposition='top'
-        [attr.theme]='theme'
-        theme='{{theme}}'
-        lang='en'
-        loading='lazy'
-      ></giscus-widget>`));
 
-    this.renderer.setProperty(this.commentsEl.nativeElement, 'innerHTML', giscusComponent);
+    if (isPlatformServer(_platformId)) {
+      const giscusComponent = collectResultSync(render(html`
+        <giscus-widget
+          id='comments'
+          repo='vendelieu/blog-frontend'
+          repoid='R_kgDOHo0zig'
+          category='Comments'
+          categoryid='DIC_kwDOHo0zis4CXkRl'
+          mapping='og:title'
+          data-strict='1'
+          reactionsenabled='1'
+          emitmetadata='0'
+          inputposition='top'
+          [attr.theme]='theme'
+          theme='{{theme}}'
+          lang='en'
+          loading='lazy'
+        ></giscus-widget>`));
+
+      this.renderer.setProperty(this.commentsEl.nativeElement, 'innerHTML', giscusComponent);
+    }
     themeService.getFlow().subscribe((e) => {
       this.theme = e;
     });
